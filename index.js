97d59f3bb9c44a107c1867de2e7f3adc3928ffc5
NawrasAppDev <mohammedzeyadalharbi9@gmail.com>
2025-04-12 18:02:32 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px', fontFamily: 'Arial' }}>
+      <h1>Welcome to Nawras</h1>
+      <p>Your school app is live and working perfectly!</p>
+    </div>
+  );
+}
