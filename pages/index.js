b5bf6c99c7f15dcaf00200645988ea522b4ad02d
NawrasAppDev <mohammedzeyadalharbi9@gmail.com>
2025-04-12 17:42:58 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>Welcome to Nawras!</h1>
+      <p>Your school app is live and ready.</p>
+    </div>
+  );
+}
